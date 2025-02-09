727fa13fefdaef47515006fc2736aa55122c7245
Faris Khalifa <73788668+phppho@users.noreply.github.com>
2025-02-10 00:43:13 +0100

    update app.py

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ if __name__ == '__main__':
     import threading
     thread = threading.Thread(target=run_scheduler)
     thread.start()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 4000)))
